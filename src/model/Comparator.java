9d050353b08f4b2028328a730367b902f4829923
dasiuss <dasiuss@gmail.com>
2014-11-27 16:09:06 +0100

    fast copy back if file is younger (will not copy new files back!) displaying errors in logger

--- src/model/Comparator.java
+++ src/model/Comparator.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.google.common.io.Files;
 
@@ -15,6 +17,8 @@ public class Comparator extends Thread{
 	private PrintStream logger;
 	private int scanned = 0;
 	private int copied = 0;
+	private int copiedBack = 0;
+	private List<String> errorsList;
 
 	public Comparator(String source, String destination) throws FileNotFoundException{
 		this.sourcePath = source;
@@ -22,6 +26,7 @@ public class Comparator extends Thread{
 
 		this.files = new FilesInputStream(source);
 		logger = System.out;
+		errorsList = new LinkedList<String>();
 	}
 
 	public Comparator(String source, String destination, PrintStream logger) throws FileNotFoundException{
@@ -49,13 +54,27 @@ public class Comparator extends Thread{
 					destFile.getParentFile().mkdirs();
 					Files.copy(sourceFile, destFile);
 				} catch (IOException e) {
+					errorsList.add(e.getMessage());
+					continue;
+				}
+			}else if(isOlder(sourceFile , destFile)){
+				copiedBack++;
+				try {
+					Files.copy(destFile, sourceFile);
+				} catch (IOException e) {
+					errorsList.add(e.getMessage());
 					e.printStackTrace();
 					continue;
 				}
 			}
 		}
+		logger.println("E R R O R S :");
+		for (String error : errorsList){
+			logger.println(error);
+		}
 		logger.println("scanned: " + scanned);
 		logger.println("copied: " + copied);
+		logger.println("errors: " + errorsList.size());
 		logger.println("no more files found");
 		long time = System.currentTimeMillis() - startTime;
 		logger.println("That took "+ time/60000 +" min (" + time + "ms)");
